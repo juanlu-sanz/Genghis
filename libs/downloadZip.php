97b17df33bc78dc5161717df900a27bc9d701f04
kadaki01 <juanljr2@gmail.com>
2013-05-19 13:36:37 +0200

    Now it downloads zips properly, fixed bug there

--- libs/downloadZip.php
+++ libs/downloadZip.php
@@ -2,12 +2,6 @@
 
 $DIR = '/var/lib/tomcat6/webapps/khan/exercises/';
 
-
-
-$fh = fopen($DIR . $name . ".html", 'w') or exit("Unable to open file!");
-fwrite($fh, $super_String);
-fclose($fh);
-
 $files = scandir($DIR);
 $final_files = array();
 $names = array();
@@ -19,18 +13,28 @@ foreach ($files as $file) {
     }
 }
 
-$zipname = $DIR . 'exercises.zip';
+$date = new DateTime();
+$d = $date->format('_Y-m-d_H:i:s');
+
+$zipname = $DIR . 'exercises'.$d.'.zip';
+
+/*
+echo '<pre>';
+print_r($final_files);
+print_r($names);
+echo '</pre>';
+ */
 
 $zip = new ZipArchive;
 $zip->open($zipname, ZipArchive::CREATE);
-for ($i = 0; $i < count($names); $i++) {
-    $zip->addFile($final_files[i], $names[i]);
+foreach ($final_files as $file) {
+    $zip->addFile($file);
 }
 $zip->close();
 
 
 header('Content-Type: application/zip');
-header('Content-disposition: attachment; filename=exercises.zip');
+header('Content-disposition: attachment; filename=exercises'.$d.'.zip');
 header('Content-Length: ' . filesize($zipname));
 readfile($zipname);
 
