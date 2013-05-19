35c8bd1566b1ae7be35dc15976c313fb7d812918
kadaki01 <juanljr2@gmail.com>
2013-05-19 13:02:19 +0200

    Now you can download a zip with all html files

--- libs/downloadZip.php
+++ libs/downloadZip.php
@@ -8,14 +8,23 @@ $fh = fopen($DIR . $name . ".html", 'w') or exit("Unable to open file!");
 fwrite($fh, $super_String);
 fclose($fh);
 
+$files = scandir($DIR);
+$final_files = array();
+$names = array();
 
-$files = array('libs/createExercise.php', 'libs/editVariable.php');
+foreach ($files as $file) {
+    if($file[2] == '_'){
+        array_push($final_files, $DIR.$file);
+        array_push($names, $file);
+    }
+}
+
 $zipname = $DIR . 'exercises.zip';
 
 $zip = new ZipArchive;
 $zip->open($zipname, ZipArchive::CREATE);
-foreach ($files as $file) {
-    $zip->addFile($file);
+for ($i = 0; $i < count($names); $i++) {
+    $zip->addFile($final_files[i], $names[i]);
 }
 $zip->close();
 
@@ -24,3 +33,4 @@ header('Content-Type: application/zip');
 header('Content-disposition: attachment; filename=exercises.zip');
 header('Content-Length: ' . filesize($zipname));
 readfile($zipname);
+
